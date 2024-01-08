e51e6218b44d88a01e69589fbf2e33193c0bad19
Bono <kubonkwan99@gmail.com>
2024-01-08 17:06:35 +0800

    style: improve ui/ux

--- src/components/slideEditor/Item/ItemControl.tsx
+++ src/components/slideEditor/Item/ItemControl.tsx
@@ -30,7 +30,7 @@ const ItemControl = React.memo(({ itemId }: { itemId: string }) => {
   return (
     <div
       id="item-control--wrapper"
-      className="border-accent-light rounded-md border-2 w-[300px] min-w-[182px] h-10 absolute bg-primary-white -top-12 right-0 group-hover/resize:opacity-100 transition-opacity opacity-0 flex"
+      className="border-accent-light rounded-md border-2 w-[250px] min-w-[120px] h-10 absolute bg-primary-white -top-12 right-0 group-hover/resize:opacity-100 transition-opacity opacity-0 flex scale-0 group-hover/resize:scale-100 delay-200"
     >
       <div
         role="button"
@@ -52,9 +52,9 @@ const ItemControl = React.memo(({ itemId }: { itemId: string }) => {
         role="button"
         tabIndex={0}
         onClick={handleDelete}
-        className="border-accent-mid/50 text-xl px-4 py-1 flex-shrink-0 cursor-pointer"
+        className="border-accent-mid/50 text-xl px-2 py-1 flex-shrink-0 cursor-pointer"
       >
-        Delete
+        ❌
       </div>
     </div>
   );
